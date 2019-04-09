515117bf6d4e0d80e50a6e272d85b45dede00868
AkshaY2039 <ced15i031@iiitdm.ac.in>
2019-04-09 10:27:13 +0530

    Lab Exam Done

---
+++ End_Sem_Lab_Exam/.tmp_versions/KeyloggerRev.mod
@@ -0,0 +1,3 @@
+/home/akshay/Desktop/SEM8LAB/Device_Drivers/End_Sem_Lab_Exam/KeyloggerRev.ko
+/home/akshay/Desktop/SEM8LAB/Device_Drivers/End_Sem_Lab_Exam/KeyloggerRev.o
+
--- End_Sem_Lab_Exam/ASCII.sh
+++ End_Sem_Lab_Exam/ASCII.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# Write a shell program which takes input as a character and prints output as its ASCII value.
+
 echo "The ASCII Value of " $1 " is"
-LC_CTYPE=C printf '%d\n' "'$1"
+# LC_CTYPE=C
+printf '%d\n' "'$1"
